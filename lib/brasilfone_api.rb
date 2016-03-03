4831b9bd6fab031e073c9cebaf03e85b1cd42ea6
Victor Fonseca <victorcel19@gmail.com>
2016-03-03 19:05:00 -0300

    Adds base module
    Base module with functions that represent endpoints from the web api

---
+++ lib/brasilfone_api.rb
@@ -0,0 +1,49 @@
+require 'brasilfone_api/version'
+require 'brasilfone_api/configuration'
+require 'brasilfone_api/http_api_handler'
+
+module BrasilfoneAPI
+  class << self
+    def configure(&block)
+      BrasilfoneAPI::Configuration.configure(&block)
+    end
+
+    def config
+      BrasilfoneAPI::Configuration.config
+    end
+
+    def send_sms(destination, text)
+      HttpAPIHandler.send_request_for_service(
+        :SEND_SMS,
+        destination: destination,
+        text: text
+      )
+    end
+
+    def sms_status(sms_id)
+      HttpAPIHandler.send_request_for_service(
+        :GET_SMS_STATUS,
+        id: sms_id
+      )
+    end
+
+    def balance
+      HttpAPIHandler.send_request_for_service(:GET_BALANCE)
+    end
+
+    def sms_status_by_date_range(start_date, end_date)
+      HttpAPIHandler.send_request_for_service(
+        :GET_STATUS_BY_DATE,
+        start_date: start_date,
+        end_date: end_date
+      )
+    end
+
+    def reply_status_for_sms(sms_id)
+      HttpAPIHandler.send_request_for_service(
+        :GET_SMS_STATUS,
+        id: sms_id
+      )
+    end
+  end
+end
